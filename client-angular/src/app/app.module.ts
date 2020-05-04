99cf4cbce37624b725215c369b249f8c93a73ad8
gn-exia <djamelmiloudi3@gmail.com>
2020-05-04 14:49:01 +0200

    first commit

---
+++ client-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client-angular/src/app/users-list/users-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "../services/data-service/data.service";
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  users: any[];
+
+  ngOnInit() {
+    this.dataService.fetchUsers()
+      .subscribe(
+        res => {
+          // console.log(res);
+          this.users = res;
+        },
+        error => {
+          console.log(error);
+
+        }
+      );
+
+  };
+}
